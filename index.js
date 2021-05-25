7f4ad4e21da7ffecaeb02384843259b0fed95bd6
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-05-25 19:31:18 +0600

    parsed url and req

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 //module scaffolding
 const app = {};
@@ -23,7 +25,23 @@ app.createServer = () => {
    });
 };
 app.handleReqRes = (req, res) => {
-   res.end('Hello Programmers');
+   //parsing requests
+   const { pathname, query } = url.parse(req.url, true);
+   const path = pathname.replace(/^\/+|\/+$/g, '');
+   const method = req.method.toLowerCase();
+   const headers = req.headers;
+
+   const decoder = new StringDecoder();
+   let data = '';
+
+   req.on('data', (buffer) => {
+      data += decoder.write(buffer);
+   });
+   req.on('end', () => {
+      data += decoder.end();
+      console.log(data);
+      res.end('Hello World');
+   });
 };
 
 // starting the server
